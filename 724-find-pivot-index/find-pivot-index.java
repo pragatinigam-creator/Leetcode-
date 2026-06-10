7c1fcfcb08e80c49536cf72c1987a28cc72ff860
pragatinigam-creator <pragatinigam14@gmail.com>
2026-06-10 22:52:33 +0530

    Time: 1 ms (98.41%) | Memory: 47.6 MB (21.77%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -1,22 +1,21 @@
+// Runtime: 1 ms, faster than 92.94% of Java online submissions for Find Pivot Index.
+// Time Complexity : O(n)
 class Solution {
     public int pivotIndex(int[] nums) {
-        int totalSum=0;
-        for(int num:nums){
-            totalSum+=num;
-
-
-        }
-        int leftSum=0;
-        for(int i=0;i<nums.length;i++){
-            int rightSum=totalSum-nums[i]-leftSum;
-            if (leftSum==rightSum){
-                return i;
-            }
-            leftSum+=nums[i];
-        }
-        return -1;
-
-
-        
+        // Initialize total sum of the given array...
+        int totalSum = 0;
+        // Initialize 'leftsum' as sum of first i numbers, not including nums[i]...
+        int leftsum = 0;
+        // Traverse the elements and add them to store the totalSum...
+        for (int ele : nums)
+            totalSum += ele;
+        // Again traverse all the elements through the for loop and store the sum of i numbers from left to right...
+        for (int i = 0; i < nums.length; leftsum += nums[i++])
+            // sum to the left == leftsum.
+            // sum to the right === totalSum - leftsum - nums[i]..
+            // check if leftsum == totalSum - leftsum - nums[i]...
+            if (leftsum * 2 == totalSum - nums[i])
+                return i;       // Return the pivot index...
+        return -1;      // If there is no index that satisfies the conditions in the problem statement...
     }
 }
