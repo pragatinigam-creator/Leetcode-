69f2667e4d44a8e7b010ff7020dc8f94a8a92ec7
Pragati Nigam <pragatinigam14@gmail.com>
2026-08-24 00:33:15 +0530

    Time: 8 ms (53.28%) | Memory: 46.9 MB (83.94%) - LeetSync

--- 2039-sum-game/sum-game.java
+++ 2039-sum-game/sum-game.java
@@ -1,18 +1,24 @@
 class Solution {
-  public boolean sumGame(String num) {
-    final int n = num.length();
-    double ans = 0.0;
+    public boolean sumGame(String num) {
+        int n = num.length();
 
-    for (int i = 0; i < n / 2; ++i)
-      ans += getExpectation(num.charAt(i));
+        int leftSum = 0, rightSum = 0;
+        int leftQ = 0, rightQ = 0;
 
-    for (int i = n / 2; i < n; ++i)
-      ans -= getExpectation(num.charAt(i));
+        for (int i = 0; i < n / 2; i++) {
+            if (num.charAt(i) == '?')
+                leftQ++;
+            else
+                leftSum += num.charAt(i) - '0';
+        }
 
-    return ans != 0.0;
-  }
+        for (int i = n / 2; i < n; i++) {
+            if (num.charAt(i) == '?')
+                rightQ++;
+            else
+                rightSum += num.charAt(i) - '0';
+        }
 
-  private double getExpectation(char c) {
-    return c == '?' ? 4.5 : c - '0';
-  }
+        return (leftSum - rightSum) * 2 != (rightQ - leftQ) * 9;
+    }
 }
