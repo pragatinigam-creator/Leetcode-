565a9a6a3e27b1486b9ab8244633ed2ca360266c
Pragati Nigam <pragatinigam14@gmail.com>
2026-09-10 16:13:11 +0530

    Time: 17 ms (83.66%) | Memory: 97.3 MB (99.06%) - LeetSync

--- 77-combinations/combinations.java
+++ 77-combinations/combinations.java
@@ -1,37 +1,23 @@
-import java.util.*;
-
-public class Solution {
-    List<List<Integer>> res = new ArrayList<>();
-
-    void solve1(int num, int tot, int k, List<Integer> ans) {
-        if (num == tot + 1) {
-            if (ans.size() == k) {
-                res.add(new ArrayList<>(ans));
-            }
-            return;
-        }
-
-        ans.add(num);
-        solve1(num + 1, tot, k, ans);
-        ans.remove(ans.size() - 1);
-        solve1(num + 1, tot, k, ans);
-    }
-
-    void solve2(int num, int tot, int k, List<Integer> ans) {
-        if (ans.size() == k) {
-            res.add(new ArrayList<>(ans));
-            return;
-        }
-        for (int i = num; i <= tot; i++) {
-            ans.add(i);
-            solve2(i + 1, tot, k, ans);
-            ans.remove(ans.size() - 1);
-        }
-    }
-
+class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+    
     public List<List<Integer>> combine(int n, int k) {
-        List<Integer> ans = new ArrayList<>();
-        solve2(1, n, k, ans);
-        return res;
+        List<Integer> list = new ArrayList<>();
+        helper(n, k, 0, list);
+        return ans;
+    }
+
+    void helper(int n, int k, int num, List<Integer> list) {
+        if (k == 0) {
+            // If we've picked k numbers, add the combination to answer
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+        
+        for (int i = num + 1; i <= n; i++) {
+            list.add(i);                       // Choose the number
+            helper(n, k - 1, i, list);         // Explore further
+            list.remove(list.size() - 1);      // Backtrack
+        }
     }
 }
